e414e0fbf13c314f34cba1405d743aa38aaf97ea
unknown <phyinpholuwa97@gmail.com>
2022-06-14 14:45:02 -0700

    dist commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -44,81 +44,9 @@ export class DashboardComponent implements OnInit {
     this.allContacts=localStorage.localUser?JSON.parse(localStorage.localUser):[]
     this.userArray = this.allContacts[this.Currentlog]
     this.userFirstName = this.userArray.firstName
-    this.userAccountNumber =this.userArray.accountNumber
-    this.totalAmount=this.allContacts[this.Currentlog].totalAmount
-    this.userWallets=this.allContacts[this.Currentlog].wallet
-    this.AcctBalance=this.allContacts[this.Currentlog].AcctBalance
-    this.userHistory = this.allContacts[this.Currentlog].history
-    this.reversedHistory = this.userHistory.reverse()
-    this.reversedSlicedHistory = this.reversedHistory.slice(0,3)
-    this.requestsArray= this.allContacts[this.Currentlog].requests
-    this.reversedRequestsArray = this.requestsArray.reverse()
-    this.reversedSlicedRequests = this.reversedRequestsArray.slice(0,3)
-    console.log(this.userWallets)
-  }
-
-
-
-  fundA(){
-    console.log(`ready to fund`)
-    // this.router.navigate(["dashboard/fund-account"])
-  }
-  transferA(){
-    this.router.navigate(["/dashboard/transfer"])
-  }
-  createW(){
-    this.router.navigate(["/dashboard/create-wallet"])
-  }
-
-  requestF(){
-    this.router.navigate(["/dashboard/request-funds"])
-  }
-
-
-  fundMe(){
-    console.log(this.fundAmount)
-    if(this.fundAmount){
-       //Initialise date and time display settings
-        var myDate = new Date;
-        var allTime = myDate.toLocaleTimeString()
-        var allDate =  myDate.toLocaleDateString()
-
-      //Add money to the main account
-      this.allContacts[this.Currentlog].mainAccountAmount+=parseFloat(this.fundAmount)
-      //Add money to the total amount
-      this.totalAmount+=parseFloat(this.fundAmount)
-      this.userArray.totalAmount=this.totalAmount
-      //Generate Reference number
-      this.referenceNumber = Math.round(Math.random()*10000000000000000)
-      //Push to history
-      this.fundAccountHistory = {
-        nameOfReceiver: "Raymond Bank",
-        accountNumberOfReceiver:"Debit Card Number",
-        date:allDate,
-        time:allTime,
-        description:"Visa Debit Card withdrawal from Raymond Bank",
-        transferAmount:parseFloat(this.fundAmount),
-        transactionType:'credit',
-        referenceNumber:this.referenceNumber
-      }
-      this.allContacts[this.Currentlog].history.push(this.fundAccountHistory)
-      //push to local storage
-      localStorage.setItem("localUser",JSON.stringify(this.allContacts))
-      alert(`Account Funded Successfully`)
-      this.ngOnInit();
-      // this.router.navigate(["/dashboard"])
-    }
-    else{
-      alert("Kindly Enter An Amount")
-    }
 
   }
-  routeToMessages(){
-    this.router.navigate(['/messages'])
-  }
-  navigateToTransactions(){
-    this.router.navigate(['/history'])
-  }
+
 
 }
 
